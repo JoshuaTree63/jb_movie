8bba0044e53e9b4e3d5b6f15bc1f5d8dcad13e0a
Joshua Levi <joshua.levi63@gmail.com>
2023-04-03 08:51:31 +0300

    in the resturent - JER

--- imdb_app/models.py
+++ imdb_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import RESTRICT
 from django.core.validators import MaxValueValidator, MinValueValidator
-from datetime
 
 
 class Movie(models.Model):
